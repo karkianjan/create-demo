1bab24891e6096ff6500cf4aec468cc07b19b97b
karkianjan <carkey.anjan2018@gmail.com>
2024-03-17 12:57:51 +0545

    change

--- src/lifting/parent.js
+++ src/lifting/parent.js
@@ -1,27 +1,25 @@
-import React from 'react'
-import Liftingup from './liftingup';
+import React from "react";
+import Liftingup from "./liftingup";
 
+class Parent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { temperature: "" };
+  }
 
- class Parent extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {temperature: ''};
-      }
-
-      handleChange = (temp) =>{
-this.setState({temperature: temp})
-      }
+  handleChange = (temp) => {
+    this.setState({ temperature: temp });
+  };
   render() {
     return (
-
       <div>
-        <h1>Parent</h1>
         <p> {this.state.temperature} </p>
-        <Liftingup temperature={this.state.temperature}  onClickTemperature={this.handleChange}/>
+        <Liftingup
+          temperature={this.state.temperature}
+          onClickTemperature={this.handleChange}
+        />
       </div>
-      
-    
-    )
+    );
   }
 }
-export default Parent
+export default Parent;
